fdcc6b5c2dc94ade2240d634e1334eaa146474d3
christoffer <christoffer@ansur.no>
2021-02-18 23:32:11 +0100

    Sparklines are now filled with a color. Also changed to d3.area and added margin to achive corerct fill without left and right vertical lines showing.

--- src/components/SparkLine.js
+++ src/components/SparkLine.js
@@ -1,28 +1,27 @@
 import React, { useEffect, useRef } from 'react';
-import { select, min, max, line, scaleLinear } from "d3";
+import { select, min, max, area, scaleLinear } from "d3";
 import './SparkLine.scss';
 
-const makeChart = ({ data, width, height, parent }) => {
-  const x = scaleLinear()
-    .domain([0, data.length])
-    .range([0, width]);
-  
-  const y = scaleLinear()
-    .domain([min(data), max(data)])
-    .range([height, 0]);    
-  
-  const svg = select(parent)
-    .append('svg:svg')
-    .attr('width', width)
+const makeChart = ({ data, width, height, element }) => {
+  const margin = 2;
+  const svg = select(element)
+    .append('svg')
+    .attr('width', width - 2 * margin)
     .attr('height', height)
-    .attr('class', 'sparkline');
+    .attr('class', 'sparkline')
+    .append("g")
+    .attr("transform", "translate(" + (-margin) + ",0)");
 
-  const curve = line()
-    .x((d, i) => x(i))
-    .y((d, i) => y(d));
+  const x = scaleLinear().domain([0, data.length]).range([0, width]);
+  const y = scaleLinear().domain([min(data), max(data)]).range([height, 0]);    
 
-  svg.append('svg:path')
-    .attr('d', curve(data));
+  svg.append('path')
+    .datum(data)
+    .attr('d', area()
+      .x((d, i) => x(i))
+      .y0(y(0))
+      .y1((d, i) => y(d))
+    );
 };
 
 const SparkLine = (props) => {
@@ -33,7 +32,7 @@ const SparkLine = (props) => {
       data: props.data || [],
       width: props.width || 135,
       height: props.height || 50,
-      parent: ref.current      
+      element: ref.current      
     });
   }, [ref]);
   
