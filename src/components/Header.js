6609fd2396573906d575f18c7dafdad5dbec35c0
christoffer <christoffer@ansur.no>
2021-02-18 21:43:44 +0100

    Replaced giraff png logo with new design in svg

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import giraffeSmall from '../assets/giraffe_small.png';
+import giraffeSmall from '../assets/giraffe.svg';
 import './Header.scss';
 
 const Header = () => {
