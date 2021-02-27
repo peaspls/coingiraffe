aae354fc27b297a0858bfad5447089325d55e23d
christoffer <christoffer@ansur.no>
2021-02-27 19:19:31 +0100

    Change toggle background colors

--- src/components/CurrencyToggle.js
+++ src/components/CurrencyToggle.js
@@ -13,8 +13,8 @@ const CurrencyToggle = (props) => {
       <Toggle 
         offText="EUR"
         onText="USD" 
-        offBackgroundColor="rgb(63,60,171)"
-        onBackgroundColor="rgb(113,173,60)"
+        offBackgroundColor="rgb(34,32,121)"
+        onBackgroundColor="rgb(74,121,32)"
         offTextColor="rgb(255,255,255)"
         onTextColor="rgb(255,255,255)"
         nobColor="rgb(255,255,255)"
