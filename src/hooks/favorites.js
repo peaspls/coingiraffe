0ce6c9d8de380eafc6bbd03ac9b195ce61c9623d
christoffer <christoffer@ansur.no>
2021-02-27 19:13:24 +0100

    favorites hook now uses localstorage hook to avoid duplication

--- src/hooks/favorites.js
+++ src/hooks/favorites.js
@@ -1,22 +1,12 @@
-import { useState } from 'react';
-
-const getFromLocalStorage = () => {
-  const favorites = window.localStorage.getItem('favorites');
-  return favorites !== null ? JSON.parse(favorites) : {};
-};
-
-const saveToLocalStorage = (favorites) => {
-  window.localStorage.setItem('favorites', JSON.stringify(favorites));
-};
+import { useLocalStorage } from './localStorage';
 
 const useFavorites = () => {
-  const [favorites, setFavorites] = useState(getFromLocalStorage());
+  const [favorites, setFavorites] = useLocalStorage({ key: 'favorites', defaultValue: {}});
 
   const toggleFavorite = (id) => {
     const change = { ...favorites };
     change[id] === undefined ? change[id] = true : delete change[id];    
     setFavorites(change);
-    saveToLocalStorage(change);
   };
 
   return [favorites, toggleFavorite];
