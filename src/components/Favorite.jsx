f899aa995aca80daa2dcca852f2e70a086ae0490
Christoffer Thondrup <thondrup@pm.me>
2022-12-04 19:26:28 +0100

    replaced favorite button with mui button

--- src/components/Favorite.jsx
+++ src/components/Favorite.jsx
@@ -1,18 +1,12 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import IconButton from '@mui/material/IconButton';
 import { createUseStyles } from 'react-jss'
 import FavoriteBorderRoundedIcon from '@mui/icons-material/FavoriteBorderRounded';
 import FavoriteRoundedIcon from '@mui/icons-material/FavoriteRounded';
 
 const useStyles = createUseStyles({
   favorite: {
-    cursor: 'pointer',
-    background: 'none',
-    border: 'none',
-    padding: 5,
-    margin: '0 10px 0 0',
-    '&:focus': {
-      outline: 'none'
-    }
+    margin: '0 10px 0 0 !important',
   }
 });
 
@@ -20,9 +14,9 @@ const Favorite = (props) => {
   const cls = useStyles();
 
   return (
-    <button className={cls.favorite} onClick={(e) => props.onClick(e)}>
+    <IconButton className={cls.favorite} aria-label="Add to favorites" onClick={(e) => props.onClick(e)}>
       {props.active ? <FavoriteRoundedIcon color="primary" /> : <FavoriteBorderRoundedIcon color="action" />}
-    </button>
+    </IconButton>
   );
 }
 
