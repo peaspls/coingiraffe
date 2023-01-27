a18db41a16ec1acb71b0965974022fec8b4ff3a3
Christoffer Thondrup <thondrup@pm.me>
2023-01-27 11:38:05 +0100

    created separate carousel comp

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react';
+import { createUseStyles } from 'react-jss'
+import useEmblaCarousel from 'embla-carousel-react';
+
+const useStyles = createUseStyles({
+  embla: {
+    overflow: 'hidden',
+  },
+  emblaContainer: {
+    display: 'flex',
+  },
+  emblaSlide: {
+    flex: '0 0 100%',
+    minWidth: 0,
+    overflowX: 'auto',
+    height: 'calc(100vh - 161px)'
+  },
+});
+
+export default function Carousel(props) {
+  const cls = useStyles();
+  const { children, onChange, value, options } = props;
+  const [emblaRef, emblaApi] = useEmblaCarousel(options)
+  const [slide, setSlide] = useState(value);
+
+  useEffect(() => {
+    const onSelect = () => {
+      const slide = emblaApi.selectedScrollSnap();
+      setSlide(slide);
+
+      if (onChange !== undefined) {
+        onChange(slide);
+      }
+    }
+
+    if (emblaApi) {
+      emblaApi.on('select', onSelect);
+    }
+
+    return () => {
+      if (emblaApi) {
+        emblaApi.off('select', onSelect);
+      }
+    }
+  }, [emblaApi])
+
+  useEffect(() => {
+    if (emblaApi && slide !== value) {
+      const jump = true
+      emblaApi.scrollTo(value, jump);
+    }
+  }, [value]);
+
+  return (
+    <div className={cls.embla} ref={emblaRef}>
+      <div className={cls.emblaContainer}>
+        {children.map((slide, index) => (
+          <div key={index} className={cls.emblaSlide}>
+            {slide}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/pages/MarketsPage.jsx
+++ src/pages/MarketsPage.jsx
@@ -1,6 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import { createUseStyles } from 'react-jss';
-import useEmblaCarousel from 'embla-carousel-react';
+import React, { useState } from 'react';
 import FavoriteRoundedIcon from '@mui/icons-material/FavoriteRounded';
 import ShowChartRoundedIcon from '@mui/icons-material/ShowChartRounded';
 import Tab from '@mui/material/Tab';
@@ -10,80 +8,30 @@ import { useLocalStorage } from '../hooks/localStorage';
 import { useMarkets } from '../hooks/markets';
 import FixedBottomTabs from '../components/FixedBottomTabs';
 import MarketsList from '../components/MarketsList';
-
-const useStyles = createUseStyles({
-  embla: {
-    overflow: 'hidden',
-  },
-  emblaContainer: {
-    display: 'flex',
-  },
-  emblaSlide: {
-    flex: '0 0 100%',
-    minWidth: 0,
-    overflowX: 'auto',
-    height: 'calc(100vh - 161px)'
-  },
-});
+import Carousel from '../components/Carousel';
 
 export default function MarketsPage() {
-  const cls = useStyles();
-  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false, speed: 20 })
-  const [tabIndex, setTabIndex] = useState(0);
+  const [tab, setTab] = useState(0);
   const [fiat, setFiat] = useLocalStorage({ key: 'fiat', defaultValue: 'usd' });
   const [markets, updateMarkets] = useMarkets({ fiat });
   const [favorites, toggleFavorite] = useFavorites();
 
-  // Set tabIndex on tab change
-  const handleTabIndexChange = (tabIndex) => {
-    setTabIndex(tabIndex);
-
-    if (emblaApi) {
-      const jump = true
-      emblaApi.scrollTo(tabIndex, jump);
-    }
-  }
-
-  // Set tabIndex on carousel change
-  useEffect(() => {
-    const onSelect = () => {
-      const tabIndex = emblaApi.selectedScrollSnap();
-      setTabIndex(tabIndex);
-    }
-
-    if (emblaApi) {
-      emblaApi.on('select', onSelect);
-    }
-
-    return () => {
-      if (emblaApi) {
-        emblaApi.off('select', onSelect);
-      }
-    }
-  }, [emblaApi])
-
   return (
     <Box>
-      <FixedBottomTabs value={tabIndex} onChange={handleTabIndexChange}>
+      <FixedBottomTabs value={tab} onChange={setTab}>
         <Tab label="Markets" value={0} icon={<ShowChartRoundedIcon />} />
         <Tab label="Favorites" value={1} icon={<FavoriteRoundedIcon />} />
       </FixedBottomTabs>
 
-      <div className={cls.embla} ref={emblaRef}>
-        <div className={cls.emblaContainer}>
-          <div className={cls.emblaSlide}>
-            <MarketsList
-              fiat={fiat}
-              markets={markets}
-              favorites={favorites}
-              onToggleFavorite={toggleFavorite}
-            />
-          </div>
-          <div className={cls.emblaSlide}>
-            Favorites
-          </div>
-        </div>
-      </div>
+      <Carousel value={tab} onChange={setTab} options={{ loop: false, speed: 20 }}>
+        <MarketsList
+          fiat={fiat}
+          markets={markets}
+          favorites={favorites}
+          onToggleFavorite={toggleFavorite}
+        />
+        <div>Favorites</div>
+      </Carousel>
     </Box>
   );
 };
