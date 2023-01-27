9f30e272385b60e1843661f8ba5f8e0cc11f2e47
Christoffer Thondrup <thondrup@pm.me>
2023-01-27 15:00:44 +0100

    not showing react query dev tools

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from "./App";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('app')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
-      <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
 )
