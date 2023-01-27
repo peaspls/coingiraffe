2b9765cfd383a87d8d284857458892ea8aa7f337
Christoffer Thondrup <thondrup@pm.me>
2023-01-27 14:15:17 +0100

    using axios instead of fetch

--- src/api/markets.js
+++ src/api/markets.js
@@ -1,11 +1,13 @@
+import axios from "axios";
+
 const baseApiURL = 'https://api.coingecko.com/api/v3';
 
 const getMarkets = async (options) => {
   const params = new URLSearchParams(options);
   const url = `${baseApiURL}/coins/markets?${params}`;
 
-  return fetch(url)
-    .then(response => response.json())
+  const { data } = await axios.get(url)
+  return data
 };
 
 export { getMarkets };
