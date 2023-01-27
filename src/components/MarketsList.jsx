4e68800dcd941e658dc95a7621136a7f24eb162f
Christoffer Thondrup <thondrup@pm.me>
2023-01-27 22:57:45 +0100

    Using table for market list

--- src/components/MarketsList.jsx
+++ src/components/MarketsList.jsx
@@ -1,4 +1,11 @@
 import React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 import Coin from './Coin';
 import Price from './Price';
 import PriceChange from './PriceChange';
@@ -7,29 +14,60 @@ export default function MarketsList(props) {
   const { fiat, markets, favorites, onToggleFavorite } = props;
 
   return (
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Coin</TableCell>
+            <TableCell align="right">Price</TableCell>
+            <TableCell align="right">24h</TableCell>
+            <TableCell align="right">7d</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {markets?.map((row) => (
+            <Row
+              key={row.id}
+              row={row}
+              fiat={fiat}
+              favorites={favorites}
+              onToggleFavorite={onToggleFavorite} />
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+function Row(props) {
+  const { row, fiat, favorites, onToggleFavorite } = props;
+
+  return (
     <>
-      <div>Coin</div>
-      <div>Price</div>
-      <div>24h</div>
-      <div>7d</div>
-      {markets?.map(p => (
-        <div key={p.id}>
+      <TableRow>
+        <TableCell component="th" scope="row">
           <Coin
-            id={p.id}
-            name={p.name}
-            symbol={p.symbol}
-            image={p.image}
-            marketCap={p.market_cap}
-            marketCapRank={p.market_cap_rank}
+            id={row.id}
+            name={row.name}
+            symbol={row.symbol}
+            image={row.image}
+            marketCap={row.market_cap}
+            marketCapRank={row.market_cap_rank}
             fiat={fiat}
             favorites={favorites}
             onToggleFavorite={onToggleFavorite}
           />
-          <Price value={p.current_price} fiat={fiat} />
-          <PriceChange value={p.price_change_percentage_24h_in_currency} />
-          <PriceChange value={p.price_change_percentage_7d_in_currency} />
-        </div>
-      ))}
+        </TableCell>
+        <TableCell align="right">
+          <Price value={row.current_price} fiat={fiat} />
+        </TableCell>
+        <TableCell align="right">
+          <PriceChange value={row.price_change_percentage_24h_in_currency} />
+        </TableCell>
+        <TableCell align="right">
+          <PriceChange value={row.price_change_percentage_7d_in_currency} />
+        </TableCell>
+      </TableRow>
     </>
   );
 }
--- src/pages/MarketsPage.jsx
+++ src/pages/MarketsPage.jsx
@@ -16,7 +16,7 @@ export default function MarketsPage() {
   const marketQueryOptions = {
     vs_currency: 'usd',
     order: 'market_cap_desc',
-    per_page: 1,
+    per_page: 50,
     page: 1,
     sparkline: true,
     price_change_percentage: '24h,7d'
