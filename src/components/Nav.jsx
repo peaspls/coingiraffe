15e20c3e2043ba4d20e228107f3d9aabd2863e3b
Christoffer Thondrup <thondrup@pm.me>
2023-02-01 23:34:29 +0100

    Setting bottom nav as fixed to have it be shown on android while the url bar is visible. Without this the nav meny is hidden until you hide the url field

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,23 +1,39 @@
 import React, { memo } from 'react';
+import { createUseStyles } from 'react-jss';
 import Paper from '@mui/material/Paper'
 import Tabs from '@mui/material/Tabs';
 
+const useStyles = createUseStyles({
+  space: {
+    marginTop: '72px',
+  },
+  fixed: {
+    width: "100%",
+    position: "fixed",
+    bottom: 0
+  }
+});
+
 export default memo(function Nav(props) {
+  const cls = useStyles();
   const { value, onChange, children } = props;
 
   return (
-    <Paper>
-      <Tabs
-        value={value}
-        variant="fullWidth"
-        indicatorColor="primary"
-        textColor="primary"
-        onChange={(event, newValue) => {
-          onChange(newValue);
-        }}
-      >
-        {children}
-      </Tabs>
-    </Paper>
+    <>
+      <div className={cls.space}></div>
+      <Paper className={cls.fixed}>
+        <Tabs
+          value={value}
+          variant="fullWidth"
+          indicatorColor="primary"
+          textColor="primary"
+          onChange={(event, newValue) => {
+            onChange(newValue);
+          }}
+        >
+          {children}
+        </Tabs>
+      </Paper>
+    </>
   );
 })
