0bf9deb4b4364a24c813b5749cd70f86d29568d0
Christoffer Thondrup <thondrup@pm.me>
2023-02-05 19:56:54 +0100

    Added start_url to pwa

--- vite.config.js
+++ vite.config.js
@@ -20,6 +20,7 @@ export default defineConfig({
         theme_color: "#ffffff",
         background_color: "#ffffff",
         display: "standalone",
+        start_url: "/",
         icons: [
           {
             src: "android-chrome-192x192.png",
