139d1ae02e53b9a8eb0431b9748ab2a0dd9da6fa
Christoffer Thondrup <thondrup@pm.me>
2023-05-25 23:57:49 +0200

    added markets mock data and enabled it since coingecko gives cors error when on localhost

--- src/api/markets.js
+++ src/api/markets.js
@@ -1,10 +1,16 @@
 import axios from "axios";
+import config from "../config/config";
+import markets from "../mock/markets";
 
 const baseApiURL = 'https://api.coingecko.com/api/v3';
 
 const getMarkets = async (options) => {
   const params = new URLSearchParams(options);
   const url = `${baseApiURL}/coins/markets?${params}`;
+
+  if (config.mock) {
+    return markets;
+  }
 
   const { data } = await axios.get(url)
   return data
---
+++ src/config/config.js
@@ -0,0 +1,5 @@
+const config = {
+  mock: true,
+}
+
+export default config;
