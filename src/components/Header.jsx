34e9a7d7f22b1b08a361fdcfc289ecadd52b40ab
Christoffer Thondrup <thondrup@pm.me>
2023-07-17 02:18:27 +0200

    removed theme from header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import { useTheme } from '@mui/material/styles';
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import { useDarkMode } from '../hooks/darkMode';
@@ -9,7 +8,6 @@ import DarkModeSwitch from './DarkModeSwitch';
 export default function Header(props) {
   const { title } = props;
   const [darkMode, setDarkMode] = useDarkMode();
-  const theme = useTheme();
 
   return (
     <nav className="grid grid-cols-[1fr_auto_1fr] h-14 sticky top-0 px-4 items-center bg-white/100 border-b border-slate-900/10 z-10">
@@ -25,7 +23,7 @@ export default function Header(props) {
           <FormControlLabel sx={{ marginRight: 0 }} control={
             <DarkModeSwitch
               sx={{ m: 1 }}
-              checked={theme.palette.mode === 'dark'}
+              checked={darkMode}
               onChange={() => setDarkMode(!darkMode)}
             />}
           />
