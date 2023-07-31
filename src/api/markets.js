70c9fb4a634f470fcdaa51fed641c20032283b38
Christoffer Thondrup <thondrup@pm.me>
2023-07-31 23:53:30 +0200

    using markets mock with sparkline based on mock parameter

--- src/api/markets.js
+++ src/api/markets.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 import markets from "../mock/markets";
+import marketsWithSparkline from "../mock/marketsWithSparkline";
 
 const baseApiURL = "https://api.coingecko.com/api/v3";
 
 const getMarkets = async (marketParams, options) => {
   if (options?.mock) {
-    console.log("Returning mock data for markets", markets);
-    return markets;
+    const mock = marketParams.sparkline ? marketsWithSparkline : markets;
+    console.log("Returning mock data for markets", mock);
+    return mock;
   }
 
   const urlParams = new URLSearchParams(marketParams);
